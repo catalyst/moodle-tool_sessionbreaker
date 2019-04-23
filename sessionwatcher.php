082367048982b6a1af75fd68305afb494de00cdc
Brendan Heywood <brendan@catalyst-au.net>
2019-04-23 11:39:38 +1000

    simplified test

--- sessionwatcher.php
+++ sessionwatcher.php
@@ -2,21 +2,6 @@
 
 define('NO_OUTPUT_BUFFERING', true);
 require_once(__DIR__ . '/../../../config.php');
-require_login();
-$PAGE->set_url('/admin/tool/sessionbreaker/sessionwatcher.php');
-$PAGE->set_context(context_system::instance());
-$PAGE->set_pagelayout('embedded');
-$strheading = 'Watch $SESSION';
-$PAGE->set_title($strheading);
-$PAGE->set_heading($strheading);
-$PAGE->navbar->add($strheading, $PAGE->url);
 
-echo $OUTPUT->header();
+print "SIZE OF SESSION->recentsessionlocks: " . sizeof($SESSION->recentsessionlocks);
 
-\core\session\manager::write_close();
-error_log( "SIZE OF SESSION->recentsessionlocks: " . sizeof($SESSION->recentsessionlocks));
-$manager = new \core\session\manager();
-if (method_exists($manager, 'display_blocking_page')) {
-    echo $manager->display_blocking_page();
-}
-echo $OUTPUT->footer();
