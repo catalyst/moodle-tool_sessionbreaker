c1cc16ca093ce6d75ca3d6d75bdded7dbb821bcb
Brendan Heywood <brendan@catalyst-au.net>
2024-12-19 22:15:53 +1100

    Add multi parallel writer test

---
+++ multiwrite.php
@@ -0,0 +1,38 @@
+<?php
+
+define('NO_OUTPUT_BUFFERING', true);
+require_once(dirname(__FILE__) . '/../../../config.php');
+
+$count = optional_param('count', 20, PARAM_INT);
+$delay = optional_param('delay', 100, PARAM_INT);
+$countdelay = optional_param('countdelay', 500, PARAM_INT);
+$readonly = optional_param('readonly', false, PARAM_BOOL);
+
+$PAGE->set_url('/admin/tool/sessionbreaker/index.php');
+$PAGE->set_context(context_system::instance());
+$PAGE->set_pagelayout('admin');
+$strheading = 'Multi write';
+$PAGE->set_title($strheading);
+$PAGE->set_heading($strheading);
+$PAGE->navbar->add(get_string('pluginname', 'tool_sessionbreaker'), $PAGE->url);
+
+\core\session\manager::write_close();
+
+echo $OUTPUT->header();
+
+echo "<p>The session_handler_class is " . $CFG->session_handler_class . "</p>";
+
+echo "<p>This loads a bunch of iframes in parallel which all mess with different session attributes.
+<p>Each also waits for a second before completing.</p>";
+
+for ($c=1; $c<=$count; $c++) {
+
+    echo "<p>Frame $c: ";
+    echo "<iframe src='write.php?c=$c&delay=$countdelay' style='height: 40px;'></iframe>";
+
+    usleep($delay * 1000); // Enough to mostly mean the iframes load in order (not required but easier to read).
+
+}
+
+echo $OUTPUT->footer();
+
---
+++ write.php
@@ -0,0 +1,26 @@
+<?php
+
+define('NO_OUTPUT_BUFFERING', true);
+
+require_once(dirname(__FILE__) . '/../../../config.php');
+
+$delay = optional_param('delay', 500, PARAM_INT);
+$c = optional_param('c', 1, PARAM_INT);
+
+require_login();
+
+// This forces the dev tools to start showing the TTFB vs the 'Content download' parts of the request.
+echo " ";
+
+$attr = "var$c";
+
+usleep($delay * 1000); // Enough delay to expose race conditions on this muc item
+
+if ($SESSION->$attr) {
+    $SESSION->$attr = $SESSION->$attr + 1;
+} else {
+    $SESSION->$attr = 1;
+}
+
+echo " $attr is now " . $SESSION->$attr;
+
