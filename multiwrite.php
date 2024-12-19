67da37433cd69bb4b2913da8eb3fecac7d595471
Brendan Heywood <brendan@catalyst-au.net>
2024-12-19 22:26:05 +1100

    Added module for concurrent overlapping writes

--- multiwrite.php
+++ multiwrite.php
@@ -5,6 +5,7 @@ require_once(dirname(__FILE__) . '/../../../config.php');
 
 $count = optional_param('count', 20, PARAM_INT);
 $delay = optional_param('delay', 100, PARAM_INT);
+$modulo = optional_param('modulo', 20, PARAM_INT);
 $countdelay = optional_param('countdelay', 500, PARAM_INT);
 $readonly = optional_param('readonly', false, PARAM_BOOL);
 
@@ -25,14 +26,21 @@ echo "<p>The session_handler_class is " . $CFG->session_handler_class . "</p>";
 echo "<p>This loads a bunch of iframes in parallel which all mess with different session attributes.
 <p>Each also waits for a second before completing.</p>";
 
+echo '<table><tr>';
 for ($c=1; $c<=$count; $c++) {
 
+    echo '<td>';
     echo "<p>Frame $c: ";
-    echo "<iframe src='write.php?c=$c&delay=$countdelay' style='height: 40px;'></iframe>";
+    echo "<iframe src='write.php?c=$c&delay=$countdelay&modulo=$modulo' style='height: 40px;'></iframe>";
 
     usleep($delay * 1000); // Enough to mostly mean the iframes load in order (not required but easier to read).
 
+    if ($c % $modulo == 0) {
+        echo '<tr>';
+    }
+
 }
+echo '</table>';
 
 echo $OUTPUT->footer();
 
--- write.php
+++ write.php
@@ -5,6 +5,7 @@ define('NO_OUTPUT_BUFFERING', true);
 require_once(dirname(__FILE__) . '/../../../config.php');
 
 $delay = optional_param('delay', 500, PARAM_INT);
+$modulo = optional_param('modulo', 100, PARAM_INT);
 $c = optional_param('c', 1, PARAM_INT);
 
 require_login();
@@ -12,11 +13,13 @@ require_login();
 // This forces the dev tools to start showing the TTFB vs the 'Content download' parts of the request.
 echo " ";
 
+$c = ($c - 1) % $modulo + 1;
+
 $attr = "var$c";
 
 usleep($delay * 1000); // Enough delay to expose race conditions on this muc item
 
-if ($SESSION->$attr) {
+if (isset($SESSION->$attr)) {
     $SESSION->$attr = $SESSION->$attr + 1;
 } else {
     $SESSION->$attr = 1;
